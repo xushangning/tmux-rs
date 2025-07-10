e44a59ca0a7804b960f25ed6c032e1df8edef84b
xushangning <skyfiretime@gmail.com>
2025-07-10 12:50:04 +0800

    main.c: set default-shell option in main()
    - lib.rs: fix OptionsTableScope wrong values.
    - tmux.rs: fix check_shell's wrong check in starts_with("/")

--- src/compat.rs
+++ src/compat.rs
@@ -1,6 +1,8 @@
 use std::ffi::CStr;
 
 // TODO: figure out an easier way to do polyfill on other platforms.
-pub fn getprogname() -> &'static [u8] {
-    unsafe { CStr::from_ptr(libc::getprogname()) }.to_bytes()
+pub fn getprogname() -> &'static str {
+    unsafe { CStr::from_ptr(libc::getprogname()) }
+        .to_str()
+        .unwrap()
 }
