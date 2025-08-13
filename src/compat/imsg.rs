ea90dc2ddc3b66e6a6dc26f8ad664b94ebbe54bd
xushangning <skyfiretime@gmail.com>
2025-08-13 14:46:41 +0800

    compat/imsg-buffer.c: ibuf_reserve

--- src/compat/imsg.rs
+++ src/compat/imsg.rs
@@ -10,7 +10,7 @@ use nix::unistd::Pid;
 
 use crate::{
     compat::queue::tailq,
-    tmux_sys::{ibuf_dynamic, ibuf_fd_set, ibuf_free, ibuf_reserve, imsg_close, imsgbuf},
+    tmux_sys::{ibuf_dynamic, ibuf_fd_set, ibuf_free, imsg_close, imsgbuf},
 };
 
 pub const HEADER_SIZE: usize = core::mem::size_of::<Hdr>();
@@ -18,7 +18,7 @@ pub const HEADER_SIZE: usize = core::mem::size_of::<Hdr>();
 #[repr(C)]
 pub struct IBuf {
     entry: MaybeUninit<tailq::Entry<IBuf>>,
-    buf: *mut c_uchar,
+    buf: NonNull<c_uchar>,
     size: usize,
     max: usize,
     wpos: usize,
@@ -27,18 +27,51 @@ pub struct IBuf {
 }
 
 impl IBuf {
+    const FD_MARK_ON_STACK: c_int = -2;
+
+    pub fn reserve(&mut self, len: usize) -> nix::Result<NonNull<u8>> {
+        if len > usize::MAX - self.wpos {
+            return Err(nix::Error::ERANGE);
+        }
+        if self.fd == Self::FD_MARK_ON_STACK {
+            // can not grow stack buffers
+            return Err(nix::Error::EINVAL);
+        }
+
+        let new_size = self.wpos + len;
+        if new_size > self.size {
+            // check if buffer is allowed to grow
+            if new_size > self.max {
+                return Err(nix::Error::ERANGE);
+            }
+            let Some(nb) = NonNull::new(
+                unsafe { libc::realloc(self.buf.as_ptr().cast(), new_size) } as *mut u8,
+            ) else {
+                return Err(nix::Error::last());
+            };
+            unsafe {
+                // Set newly allocated memory to zero.
+                nb.add(self.size).write_bytes(0, new_size - self.size);
+            }
+            self.buf = nb;
+            self.size = new_size;
+        }
+
+        let b = unsafe { self.buf.add(self.wpos) };
+        self.wpos += len;
+        Ok(b)
+    }
+
     pub fn add(&mut self, data: &[u8]) -> nix::Result<()> {
         if data.is_empty() {
             return Ok(());
         }
 
-        let Some(b) = NonNull::new(unsafe { ibuf_reserve(self, data.len()) }) else {
-            return Err(nix::Error::last());
-        };
+        let b = self.reserve(data.len())?;
 
         unsafe {
-            data.as_ptr()
-                .copy_to_nonoverlapping(b.as_ptr().cast(), data.len());
+            b.as_ptr()
+                .copy_from_nonoverlapping(data.as_ptr(), data.len());
         }
         Ok(())
     }
